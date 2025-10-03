141bf9c036c236184930ce5cfca0601dedccfc31
ltomassini <leandroctomassini@gmail.com>
2025-10-03 16:27:29 -0300

    feat: Implement client tracking and message broadcasting in WebSocket

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,14 +1,26 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 import { MessagesWsService } from './messages-ws.service';
 
 
+
 @WebSocketGateway( { cors: true } )
-export class MessagesWsGateway {
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() wss: Server;
 
   constructor(
     private readonly messagesWsService: MessagesWsService
   ) { }
 
+  handleConnection( client: Socket ) {
+    this.messagesWsService.registerClient( client );
+    this.wss.emit( 'clients-updated', this.messagesWsService.getConnectedClients() );
+  }
+
+  handleDisconnect( client: Socket ) {
+    this.messagesWsService.removeClient( client.id );
+  }
 
 }
